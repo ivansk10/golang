1e1ba9717b4c6e052850ed0e9a00a1e1bc8ab1bd
ivansk10 <ivansaad23@gmail.com>
2024-04-19 16:13:00 -0300

    Structs

---
+++ 6 - Structs/structs.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type user struct {
+	name    string
+	age     uint8
+	address address
+}
+
+type address struct {
+	publicPlace string
+	number      uint8
+}
+
+func main() {
+
+	var user1 user
+	user1.name = "Ivan Koschelny"
+	user1.age = 28
+	fmt.Println(user1)
+
+	adress1 := address{"Rua da Paz", 30}
+
+	user2 := user{"Ivan Koschelny", 28, adress1}
+	fmt.Println(user2)
+
+	user3 := user{age: 28, address: address{"Rua da paz", 25}}
+	fmt.Println(user3)
+
+}
