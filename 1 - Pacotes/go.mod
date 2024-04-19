bbcdaf4e6b120d289289449f369f8f88730a27b4
ivansk10 <ivansaad23@gmail.com>
2024-04-19 12:19:48 -0300

    Pacote externos

--- 1 - Pacotes/go.mod
+++ 1 - Pacotes/go.mod
@@ -1,3 +1,5 @@
 module module
 
 go 1.22.2
+
+require github.com/badoux/checkmail v1.2.4 // indirect
--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"module/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("ivansaad23@gmail.com")
+	erro2 := checkmail.ValidateFormat("123.com")
+	fmt.Println(erro)
+	fmt.Println(erro2)
 }
