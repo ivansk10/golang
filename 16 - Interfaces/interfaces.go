7d6f25315c807cc749aeab00ee819be117f640a2
Ivan Koschelny <IvanSaad23@gmail.com>
2024-04-20 10:58:35 -0300

    Interfaces

---
+++ 16 - Interfaces/interfaces.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type form interface {
+	area() float64
+}
+
+func writeArea(f form) {
+	fmt.Printf("Form area is %0.2f\n", f.area())
+}
+
+type rectangle struct {
+	height float64
+	width  float64
+}
+
+func (r rectangle) area() float64 {
+	return r.height * r.width
+}
+
+func (c circle) area() float64 {
+	return math.Pi * math.Pow(c.radius, 2)
+}
+
+type circle struct {
+	radius float64
+}
+
+func main() {
+
+	r := rectangle{10, 15}
+	writeArea(r)
+
+	c := circle{10}
+	writeArea(c)
+
+}
